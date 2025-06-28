3139c9509c460e00f74d76c3b7600a8c1d5c5fdd
Donaven Bruce <91450415+DonavenJB@users.noreply.github.com>
2025-06-28 12:00:00 -0700

    Add Google Analytics script.

--- data/analytics.js
+++ data/analytics.js
@@ -1,1 +1,9 @@
-// To-Do: Implement the Analytics Module to initialize the Google Tag and track user command events.
+// Implementation of the Analytics Module to initialize the Google Tag and track user command events.
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-YMYV8Y1KFB');
+function trackEvent(category, action, label = null) {
+    gtag('event', action, {'event_category': category,'event_label': label});
+}
