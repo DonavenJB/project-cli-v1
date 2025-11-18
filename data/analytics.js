c77bf0ec8d2beb2c30deea442d942af3ebc7b20c
Donaven Bruce <91450415+DonavenJB@users.noreply.github.com>
2025-11-18 10:44:41 -0800

    Resolved revert conflict and removed JS files.

--- data/analytics.js
+++
@@ -1,7 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-YMYV8Y1KFB');
-function trackEvent(category, action, label = null) {
-    gtag('event', action, {'event_category': category,'event_label': label});
-}
